4db498a8d13e760e703dda49721ab3b95a994f1f
Karta Kusuma <kartakusuma1199@gmail.com>
2023-09-15 06:28:13 +0700

    initializing project

---
+++ src/main/java/com/github/kartakusuma/springcrudjpamysql/SpringCrudJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.kartakusuma.springcrudjpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudJpaMysqlApplication.class, args);
+	}
+
+}
